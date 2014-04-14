dfb123300c354f93a3aa609c5774ee6425981e46
Rodrigo <rcastro87@gmail.com>
2014-04-14 19:31:06 -0400

    Recive y maneja los datos del Json

--- TripTour/gen/com/example/triptour/R.java
+++ TripTour/gen/com/example/triptour/R.java
@@ -16,6 +16,7 @@ public final class R {
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
+ Default screen margins, per the Android Design guidelines. 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -24,7 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btnLogin=0x7f080000;
+        public static final int edtPass=0x7f080004;
+        public static final int edtUsuario=0x7f080002;
+        public static final int txtPass=0x7f080003;
+        public static final int txtRegistrar=0x7f080005;
+        public static final int txtUsuario=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +43,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int login=0x7f050003;
+        public static final int newUser=0x7f050006;
+        public static final int pass=0x7f050005;
+        public static final int user=0x7f050004;
     }
     public static final class style {
         /** 
@@ -58,9 +69,20 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
--- TripTour/src/com/example/triptour/MainActivity.java
+++ TripTour/src/com/example/triptour/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.triptour;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener, OnTouchListener{
 
+	TextView txtUsuario, txtPass, txtRegistrar;
+	EditText edtUsuario, edtPass;
+	Button btnLogin;
+	String ip, resultado,res,valido,usr_nombre,usr_nick;
+	InputStream is = null;
+	JSONObject json = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ip = "192.168.30.75";
+		txtUsuario = (TextView)findViewById(R.id.txtUsuario);
+		txtPass = (TextView)findViewById(R.id.txtPass);
+		txtRegistrar = (TextView)findViewById(R.id.txtRegistrar);
+		
+		edtUsuario = (EditText)findViewById(R.id.edtUsuario);
+		edtPass = (EditText)findViewById(R.id.edtPass);
+		
+		btnLogin = (Button)findViewById(R.id.btnLogin);
+		
+		btnLogin.setOnClickListener(this);
+		txtRegistrar.setOnTouchListener(this);
 	}
 
 	@Override
@@ -19,4 +65,72 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		Thread nt = new Thread() {
+			@Override
+			public void run() {
+				
+				try {
+					res = enviarPost(edtUsuario.getText().toString(), edtPass.getText().toString());
+					JSONArray jsonArray = new JSONArray(resultado);
+					for (int i = 0; i < jsonArray.length(); i++) {
+				        JSONObject jsonObject = jsonArray.getJSONObject(i);
+				        valido = jsonObject.getString("valido");
+				        if(valido.equals("1")){
+				        	usr_nombre = jsonObject.getString("usr_nombre");
+				        	usr_nick = jsonObject.getString("usr_nick");
+				        	runOnUiThread(new Runnable() {
+								@Override
+								public void run() {
+									Toast.makeText(MainActivity.this, res+"valido: "+valido+" nombre: "
+											+usr_nombre+" nick: "+usr_nick,
+											Toast.LENGTH_LONG).show();
+								}
+							});
+				        }else{
+				        	runOnUiThread(new Runnable() {
+								@Override
+								public void run() {
+									Toast.makeText(MainActivity.this, res+"Usuario invalido: ",
+											Toast.LENGTH_LONG).show();
+								}
+							});
+				        }
+				        
+				}
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			}
+		};
+		nt.start();
+	}
+
+	@Override
+	public boolean onTouch(View arg0, MotionEvent arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public String enviarPost(String usuario, String pass) {
+
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpContext localContext = new BasicHttpContext();
+		HttpPost httpPost = new HttpPost("http://192.168.0.103/servtriptour/login.php");
+		HttpResponse response = null;
+		try {
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("usuario", usuario));
+			params.add(new BasicNameValuePair("pass", pass));
+			httpPost.setEntity(new UrlEncodedFormEntity(params));
+			response = httpClient.execute(httpPost, localContext);
+			HttpEntity entity = response.getEntity();
+			resultado = EntityUtils.toString(entity, "UTF-8");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return resultado;
+	}
 }
