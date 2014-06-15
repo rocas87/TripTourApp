2f6e5a1eb9304f777fcc5d350afbbe2649cd75e8
Rodrigo <rcastro87@gmail.com>
2014-06-15 02:11:54 -0400

    Solo falta cambiar color botones y texto. agregar evaluacion de usuarios

--- TripTour/src/com/example/triptour/FindActivity.java
+++ TripTour/src/com/example/triptour/FindActivity.java
@@ -615,14 +615,14 @@ public class FindActivity extends Activity implements android.location.LocationL
 		    handle = (LocationManager)getSystemService(LOCATION_SERVICE);
 		    //Clase criteria permite decidir mejor poveedor de posicion
 		    Criteria c = new Criteria();
-		    //obtiene el mejor proveedor en función del criterio asignado
+		    //obtiene el mejor proveedor en funciï¿½n del criterio asignado
 		    //ACCURACY_FINE(La mejor presicion)--ACCURACY_COARSE(PRESISION MEDIA)
 		    c.setAccuracy(Criteria.ACCURACY_COARSE);
 		    //Indica si es necesaria la altura por parte del proveedor
 		    c.setAltitudeRequired(false);
 		    provider = handle.getBestProvider(c, true);
-		    //Se activan las notificaciones de localización con los parámetros: 
-		    //proveedor, tiempo mínimo de actualización, distancia mínima, Locationlistener
+		    //Se activan las notificaciones de localizaciï¿½n con los parï¿½metros: 
+		    //proveedor, tiempo mï¿½nimo de actualizaciï¿½n, distancia mï¿½nima, Locationlistener
 		    handle.requestLocationUpdates(provider, 60000, 5,this);
 		    //Obtiene la ultima posicion conocida por el proveedor
 		    loc = handle.getLastKnownLocation(provider);
